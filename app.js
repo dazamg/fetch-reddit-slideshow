dd96ec75da9f350b7c075334e48bffad307d82e5
dazmag <iscope09@hotmail.com>
2020-10-13 01:43:37 -0400

    reset button

--- app.js
+++ app.js
@@ -1,33 +1,77 @@
+let array = [];
 document.addEventListener("DOMContentLoaded", () => {
-    const baseURL = 'http://www.reddit.com/search.json?q='
     
     document.getElementById('form').addEventListener('submit', (e) => {
+        const baseURL = 'http://www.reddit.com/search.json?q='
         e.preventDefault()
+        const text = document.querySelector('.text')
+        document.getElementById('submit').style.visibility = "hidden";
         const inputBox = document.getElementById('go').value
+        
+        go.style.visibility = "hidden";
+        text.style.visibility = "hidden";
         // console.log(baseURL+inputBox+"+nsfw:no")
         fetch(baseURL+inputBox+'+nsfw:no')
         .then((responseData)=> {
             return responseData.json()
         })
+        // let data
+        // let slideIndex=1
+        
         .then((jsonData) => {
-            const imgArr = jsonData.data.children.map(child=>{
-                return child.data.thumbnail
+            let array = [];
+            jsonData.data.children.map(child=>{
+                return array.push(child.data.thumbnail)
             })
-            console.log(imgArr)
+            
+            array.forEach(addData)
+            
         })
         .catch((err)=>{
             console.log('Something went wrong', err)
         })
     })
+    // var index= 1;
+    
+    document.getElementById('')
+
+    const addData = (thumbnailUrl) => {
+            let pic = document.createElement('img')
+            pic.setAttribute('src', thumbnailUrl)
+            images.appendChild(pic);
+            // if(imageID %2 == 0){
+            //     pic.style.display = 'block';
+            // }
+            // imageID++;
+
+        // return thumbnailUrl(imgArr)      
+    }
+
+})
+
+document.getElementById('stop').addEventListener('click', () => {
+    document.querySelector('img')
+    while(images.firstChild) {
+        images.removeChild(images.firstChild)
+    }
+    document.querySelector('.text').style.visibility = "visible";
+    document.getElementById('go').style.visibility = 'visible';
+    document.getElementById('submit').style.visibility = "visible";
+    document.getElementById('go').value=''
+        // pic1.removeAttribute('src')
+    
 })
 
 
-
-// json.data.children.forEach((child) => {
+// jsonData.children.forEach((child) => {
 //     array.push(child.data.thumbnail)
 // })  
 
+// let imgArr = jsonData.data.children.map(child=>{
+//     return child.data.thumbnail
+// })
 
+// imgArr.forEach(addData)
 
 
 
